8c2249a4399cddc97473551b4753522b79684aa1
Noé Favier <noe.favier@outlook.com>
2023-10-04 15:37:56 +0200

    feat(init front): setup angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ToolbarComponent } from './shared/toolbar/toolbar.component';
+import { LogoComponent } from './shared/logo/logo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToolbarComponent,
+    LogoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
