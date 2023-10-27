5b5c795dac729dc79968e73e52890fa5baa926c1
Bastien BOMBARDELLA <bastien.bc@outlook.fr>
2023-10-27 15:48:09 +0200

    ✨: unpublished movie

--- src/app/admin/admin-movies/admin-movies.component.ts
+++ src/app/admin/admin-movies/admin-movies.component.ts
@@ -4,6 +4,9 @@ import {DatePipe} from "@angular/common";
 import {ActivatedRoute, Router} from "@angular/router";
 import {MovieEntity} from "../../api/models/movie-entity";
 import {MovieService} from "../../api/services/movie.service";
+import {filter, Subject, switchMap, take} from "rxjs";
+import {MatDialog} from "@angular/material/dialog";
+import {ConfirmDialogComponent, ConfirmDialogConfig} from "../../shared/ui/confirm-dialog/confirm-dialog.component";
 
 @Component({
   selector: 'app-admin-movies',
@@ -30,6 +33,7 @@ export class AdminMoviesComponent {
       pipe: new DatePipe(this.locale)
     }
   ];
+  readonly refresh: Subject<void> = new Subject<void>();
 
   get fetchMethod() {
     return this.movieService.movieControllerFindAll.bind(this.movieService);
@@ -38,10 +42,33 @@ export class AdminMoviesComponent {
   constructor(private readonly movieService: MovieService,
               @Inject(LOCALE_ID) private readonly locale: string,
               private readonly activatedRoute: ActivatedRoute,
-              private readonly router: Router) {
+              private readonly router: Router,
+              private readonly matDialog: MatDialog) {
   }
 
   handleClick(movie: MovieEntity): void {
     this.router.navigate([movie.id], {relativeTo: this.activatedRoute});
+  }
+
+  handleDelete(movie: MovieEntity): void {
+    const dialogRef = this.matDialog.open<ConfirmDialogComponent, ConfirmDialogConfig>(ConfirmDialogComponent, {
+      data: {
+        title: 'Dé-publier un film',
+        content: `Voulez-vous dé-publier le film ${movie.title} ?`,
+        confirmLabel: 'Dé-publier'
+      }
+    });
+
+    dialogRef.afterClosed()
+      .pipe(
+        take(1),
+        filter(v => v === true),
+        switchMap(_ => this.movieService.movieControllerDelete({id: movie.id}))
+      )
+      .subscribe(v => {
+        if (v) {
+          this.refresh.next();
+        }
+      });
   }
 }
